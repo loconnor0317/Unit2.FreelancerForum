f3cfc5d9b3be864cdba1a8eac3202a2cd0729c0b
Lindsey O'Connor <146387147+loconnor0317@users.noreply.github.com>
2023-11-24 17:14:57 -0800

    workshop 19
    quick outline of pages at start of workshop

---
+++ freelancers.js
@@ -0,0 +1,15 @@
+/* State */
+// Here, we define variables for the data that our program needs to remember.
+// We call this data "state" because it represents the state of our program.
+console.log
+
+
+//render(); // We call this function once to render the initial state
+
+/**
+ * Update the DOM to reflect the current state.
+ * The term "render" is often used to describe this process.
+ */
+
+
+ 
