ff3654b1797d1c411c86e1af2d1bf926b96c8b4f
Lindsey O'Connor <146387147+loconnor0317@users.noreply.github.com>
2023-11-27 15:12:56 -0800

    additional work on block19 workshop
    not complete

--- freelancers.js
+++ freelancers.js
@@ -1,15 +1,46 @@
 /* State */
-// Here, we define variables for the data that our program needs to remember.
-// We call this data "state" because it represents the state of our program.
-console.log
+// DATA VARIABLES BELOW ARE DEFINED FOR THE PROGRAM TO USE.
+    
+const name = ["Pistachio", "Fern", "Emerald", "Basil", "Sage", "Pickle", "Chartreuse", "Mint", "Lime"];
+const prices = ["25", "51", "43", "81", "43", "76", "47", "72", "64"];
+const occupation = ["gardener", "programmer", "teacher", "driver", "artist"];
+const maxNames = 9;
+const names = [
+  {
+    name: "Pistactio", 
+    occupation: "gardener",
+    price: 25,
+  },
+  {
+    name: "Fern",
+    occupation: "programmer",
+    price: 51,
+  },
+];
 
+//  interval will be q 1500 milliseconds
 
-//render(); // We call this function once to render the initial state
+const addNameIntervalId = setInterval(addName, 1500);
 
-/**
- * Update the DOM to reflect the current state.
- * The term "render" is often used to describe this process.
- */
+// 1. TO DO: ADD RENDER () HERE TO INITIALIZE STATE 
+//-(CREATE INITIAL ARRAY FOR INITIAL FL, PISTACHIO AND FERN)
+//-(CREATE RANDOM ARRAY FOR OTHER RANDOM FL,)
+// 2. TO DO: ADD F(N) TO RENDER NAMES
+// 3. TO DO: ADD F(N) TO AVERAGE PRICES AFTER Q FREELANCER NAME, AFTER THE FIRST TWO FREELANCERS.
 
+function addName() {
+  
+  const name = names[Math.floor(Math.random() * names.length)];
+  
+  const occupation = occupations[Math.floor(Math.random() * occupations.length)];
 
- 
+  const price = prices[Math.floor(Math.random() * prices.length)];
+
+  names.push({ name, occupation, price});
+}
+//TO DO: ADD RENDER()
+
+//TO DO:
+//1. ADD F(N) FOR A CLEAR INTERVAL @ MAX NAMES: 9
+
+//**DOUBLE CHECK RUBRIC CRITERION BEFORE FINAL SUBMIT */
